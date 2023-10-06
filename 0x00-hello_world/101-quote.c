73b624009a70daec8177588dfd04bdad19a18bb5
Paul95 <akinolaaa95@gmail.com>
2023-10-05 19:41:45 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* Return: Always 1 (error)
-*/
-int main(void)
+* Return:)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
-
