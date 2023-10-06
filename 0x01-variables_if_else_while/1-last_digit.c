c3ea8da0b504eb04de90687e5a0006bcb77e2e85
Paul95 <akinolaaa95@gmail.com>
2023-10-06 15:51:04 -0700

    C programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (x == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n" n, x);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
 	else if (x < 6 && x != 0)
 	{
