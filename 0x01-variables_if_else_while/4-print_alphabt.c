09afcc2ec8533d5bf44180ee3110b1619d19066e
Paul95 <akinolaaa95@gmail.com>
2023-10-07 07:56:09 -0700

    C programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'Printing alphabets omitting q and e'
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <='z'; alph++)
+	{
+		if (alph != 'e'&& alph != 'q')
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
