27bfb8461173e19abfc0ba34d7d9ddb9d4590aff
Paul95 <akinolaaa95@gmail.com>
2023-10-07 06:26:09 -0700

    C programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'Printing upper and lower case alphabets with putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++);
+	{
+		putchar(alph);
+	}
+	for (alph = 'A'; alph <= 'Z'; alph++);
+	{
+	putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
