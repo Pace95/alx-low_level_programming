2bec330b348e12bc5f26c2d1a024b8c8d6b2e81c
Paul95 <akinolaaa95@gmail.com>
2023-10-08 14:04:23 -0700

    C programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main _ Entry Point
+ *
+ * Description - ' Printing all singles digits of base ten'
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+	while (x <= 9);
+	do {
+		printf(x);
+		x++;}
+	Printf('\n');
+	return (0);
+}
