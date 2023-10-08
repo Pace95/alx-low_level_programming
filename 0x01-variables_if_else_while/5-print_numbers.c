7605cb8efd5583d9986d72b60464f60e91be3f33
Paul95 <akinolaaa95@gmail.com>
2023-10-08 14:16:57 -0700

    C programming

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,13 @@
 
 int main(void)
 {
-	int x = 0;
-	do {
-		putchar(x);
-		x++;}
-	while (x <= 9);
-	putchar('\n');
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+
+	printf('\n');
 	return (0);
 }
