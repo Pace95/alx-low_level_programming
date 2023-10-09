e95d41da398d17b6e796786a28c589c938a3d065
Paul95 <akinolaaa95@gmail.com>
2023-10-09 13:17:42 -0700

    C programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'Possible combinations of two digits'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n !=57)
+		{
+			putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
