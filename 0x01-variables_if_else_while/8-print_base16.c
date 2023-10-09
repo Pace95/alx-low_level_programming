c349b4df64bdf4689e44cb6344ede03f9a23bed1
Paul95 <akinolaaa95@gmail.com>
2023-10-09 10:39:32 -0700

    C programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'Print base16 lowercase'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	for (x = '0'; x <= '9'; x++)
+	{
+		puthar(x);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
