379c3b295a041a28dbfb0805684b6834c1192908
Paul95 <akinolaaa95@gmail.com>
2023-10-11 14:29:55 -0700

    C functions and loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 /**
  * print_sign - to print sign of number
  * @n: represent the parameter
- * Return: 0 or 1 or -1
+ * Return: 1, 0 or -1
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 			return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 			return(-1);
 	}
 }
