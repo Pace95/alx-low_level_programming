f0a84bc4ef1f36fba897a6712559f95b8009c61c
Paul95 <akinolaaa95@gmail.com>
2023-10-11 16:44:28 -0700

    C functions and loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
  */
 void times_table(void)
 {
-	 int (x, n, r;
+	 int x, n, r;
 
 	 for (x = 0; x < 10; x++)
 	 {
@@ -25,4 +25,5 @@ void times_table(void)
 	 }
 	 printf("\n");
 	 }
+	 return (0);
 }
