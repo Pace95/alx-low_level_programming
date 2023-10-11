6e3ac91bb63cfba58f68bdf84d636f8464d142ee
Paul95 <akinolaaa95@gmail.com>
2023-10-10 17:07:23 -0700

    C programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'Printing without using the standard library'
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int length;
+	for (length = 0; length < 8; length++)
+		_putchar(word[length]);
+	_putchar('\n');
+	return (0);
+}
