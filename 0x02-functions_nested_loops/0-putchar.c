163a767d8841554fcab4df49af6ffdd8058313b7
Paul95 <akinolaaa95@gmail.com>
2023-10-10 17:19:00 -0700

    C programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,7 +11,7 @@ int main(void)
 	char word[8] = "_putchar";
 	int length;
 	for (length = 0; length < 8; length++)
-		putchar(word[length]);
-	putchar('\n');
+		_putchar(word[length]);
+	_putchar('\n');
 	return (0);
 }
