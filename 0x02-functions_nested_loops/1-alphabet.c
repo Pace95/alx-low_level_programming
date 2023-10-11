df65766941290aa7865b1028d5154b4548f0db45
Paul95 <akinolaaa95@gmail.com>
2023-10-11 08:13:35 -0700

    C programming functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * main - Entry of Alphabets 
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char aph;
+	for (aph = 'a'; aph <= 'z'; aph++)
+		_putchar(aph);
+	_putchar('\n')
+		return(0);
+}
