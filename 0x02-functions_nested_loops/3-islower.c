05e409072e44fb61f8fb51e80089be691cf618ac
Paul95 <akinolaaa95@gmail.com>
2023-10-11 13:45:58 -0700

    C functions and loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 /**
  * _islower - checks for lower case
  *
- * @c: parameter for checking 
+ * @c: parameter for checking
  *
  * Return: 0 or 1 (Success)
  */
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
