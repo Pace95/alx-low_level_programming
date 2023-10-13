674978bd769bc0aa39773ed55db4f400c3a8ebc8
Paul95 <akinolaaa95@gmail.com>
2023-10-13 16:50:07 -0700

    more loops, nested and functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,10 @@ void print_most_numbers(void)
 
 	for (m = '0'; m <= '9'; m++)
 	{
-		if (m == '2' || m == '4')
-			continue;
-		else
+		if (m != '2' && m != '4')
 		{
 			_putchar(m);
 		}
 	}
 	_putchar('\n');
+}
