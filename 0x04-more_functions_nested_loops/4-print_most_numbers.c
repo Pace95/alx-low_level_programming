3261596a6258c882fdecaadf3b44896c34c649ee
Paul95 <akinolaaa95@gmail.com>
2023-10-13 16:22:46 -0700

    more loops, nested and functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print all digits excluding 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		if (m != '2' && m != '4')
+			_putchar(m);
+	}
+	putchar('\n');
