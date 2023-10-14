9a2da3004cf9e8eb60b4249e28ea6d0fed4e9977
Paul95 <akinolaaa95@gmail.com>
2023-10-13 17:31:20 -0700

    more loops, nested and functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - function to draw a straight line
+ * @n: parameter for number of times _ will print
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int x
+
+			for (x +1; x <= n; x++)
+			{
+				_putchar('_');
+			}
+		_putchar('\n');
+	}
+}
