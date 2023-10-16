c884a24f8b917e984561af1e939f3753115d234b
Paul95 <akinolaaa95@gmail.com>
2023-10-16 14:02:42 -0700

    C programming

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to rest to 98 wuth pointer
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int x = 98;
+
+		int *z;
+		z = &x;
+		_putchar("n = %d\n", *p);
+}
