c67e884e5f3d1802a58c5ac545c5017e8b63a2d4
Paul95 <akinolaaa95@gmail.com>
2023-10-16 14:46:37 -0700

    C programming

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints strings and new line
+ * @str: parameter
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		_putchar(str[x]0;
+				_putchar('\n')
+}
