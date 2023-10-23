393c9d72b1445172109091386dbc8ec916f0cac6
Paul95 <akinolaaa95@gmail.com>
2023-10-23 13:14:23 -0700

    C programming pointers continuation

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: first pointer
+ * @to: second pointer
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
