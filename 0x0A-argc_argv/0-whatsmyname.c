ed7ef5a7c763d470064f1ec6c9bc6d4d88f8895f
Paul95 <akinolaaa95@gmail.com>
2023-10-30 22:39:23 -0700

    Argument Vector and count

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print the name of a programme
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 Always (Success)
+ */
+
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
