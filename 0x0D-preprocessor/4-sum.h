1b8969276cde456b9afc2e2514fb5feb4ef58516
Paul95 <akinolaaa95@gmail.com>
2023-11-06 05:47:47 -0800

    header for sum of x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef MACROSUM_H
+#define MACROSUM_H
+/**
+ * File: 4-sum
+ * @x: first value
+ * @y: second value
+ * Desc: function-like macro SUM(x, y) that computes 
+ * the sum of the numbers x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
