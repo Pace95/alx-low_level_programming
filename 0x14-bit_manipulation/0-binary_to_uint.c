65402fa0b12c4b99bf89972f95255da302df43ed
Pace95 <akinolaaa95@gmail.com>
2023-11-24 10:07:29 -0800

    function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,14 +19,14 @@ unsigned int binary_to_uint(const char *b)
 	for (length = 0; b[length] != '\0'; length++)
 		;
 	if (length == 1 && (b[0] == '0' || b[0] == '1'))
-			return (b[0] - 48);
-	for (x = 0; b[x] != '\0'; x++)
+			return (b[0] - '0');
+	for (x = 0; x < length; x++)
 	{
 		if (b[x] != '0' && b[x] != '1')
 		return (0);
 		for (y = length - 1; y > 0; y--)
 		pow = pow * base;
-		sum = sum + (pow * (b[x] - 48));
+		sum = (sum << 1) + (b[x] - '0');
 		length--;
 		pow = 1;
 	}
