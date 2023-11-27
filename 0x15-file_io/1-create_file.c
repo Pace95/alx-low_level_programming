da355b4467f52beff0caa183617145424df58e79
Pace95 <akinolaaa95@gmail.com>
2023-11-27 15:06:01 -0800

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - a function that create a file
+ * @filename: name of file to be created
+ * @text_content: NULL terminatedstring to write to the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nol;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+	if (text_context == NULL)
+		text_content = "";
+	for (nol = 0; text_content[nol]; nol++)
+		;
+	rwr = write(fd, text_content, nol);
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
