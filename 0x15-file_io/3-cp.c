b9703fca8583de07600f2874c67cec14fa22f046
Pace95 <akinolaaa95@gmail.com>
2023-11-27 17:44:18 -0800

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  * @argv: argument vector
  * Return: Nothing
  */
-void error_file(int file_from, int file_to, char argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
